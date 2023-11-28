9e839eaadd57b45b636d5ded25e6fd048df0e3cf
myoun <aiden080605@gmail.com>
2023-11-29 01:23:31 +0900

    feat: maven publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
     id("java")
+    `maven-publish`
 }
 
 group = "app.myoun"
-version = "1.0.0"
+version = "1.1.0"
 
 repositories {
     mavenCentral()
@@ -17,4 +18,25 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks {
+    create<Jar>("sourcesJar") {
+        archiveClassifier.set("sources")
+        from(sourceSets["main"].allSource)
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("headsound") {
+            from(components["java"])
+            artifact(tasks["sourcesJar"])
+
+            groupId = rootProject.group.toString()
+            artifactId = rootProject.name
+            version = rootProject.version.toString()
+
+        }
+    }
 }
